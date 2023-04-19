e4b8be54fbd58e21741403668292fc623c678c25
muditshukla3 <muditshukla3@gmail.com>
2023-04-19 22:04:41 +0530

    Updated dependencies and added logback.

---
+++ twitter-to-kafka-service/src/main/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,30 @@
+package com.microservice.demo.twitter.to.kafka.service;
+
+import com.microservice.demo.twitter.to.kafka.service.config.TwitterToKafkaConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaConfigData twitterToKafkaConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaConfigData twitterToKafkaConfigData) {
+        this.twitterToKafkaConfigData = twitterToKafkaConfigData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("App starts....");
+        LOG.info(Arrays.toString(twitterToKafkaConfigData.getTwitterKeywords().toArray(new String[] {})));
+    }
+}
---
+++ twitter-to-kafka-service/src/test/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTest {
+
+    @Test
+    public void contextLoad(){}
+}
